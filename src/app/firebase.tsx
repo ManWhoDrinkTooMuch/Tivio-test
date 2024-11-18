74dddac53c57bf88e8412c80d85c537c07f6c397
ManWhoDrinkTooMuch <jakub.pitra@gmail.com>
2024-11-18 14:52:36 +0100

    Update firebase.tsx

--- src/app/firebase.tsx
+++ src/app/firebase.tsx
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyD4xhe4cKcukwM9lTjWykfXlHTFRhCzCLM",
-    authDomain: "tivio-auth.firebaseapp.com",
-    projectId: "tivio-auth",
-    storageBucket: "tivio-auth.firebasestorage.app",
-    messagingSenderId: "474891054304",
-    appId: "1:474891054304:web:5d2169d418324bbbe6f485"
+    apiKey: "API_KEY",
+    authDomain: "AUTH_DOMAIN",
+    projectId: "PROJECT_ID",
+    storageBucket: "BUCKET",
+    messagingSenderId: "SENDER_ID",
+    appId: "APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
